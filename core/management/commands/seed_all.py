aaf7cfd0f5380858bb2b0de8a3746fb39001b79a
cloe001000 <69475099+cloe001000@users.noreply.github.com>
2021-08-02 21:50:55 +0900

    vs settings.json 생성 , Posting구성 작업 시작

--- core/management/commands/seed_all.py
+++ core/management/commands/seed_all.py
@@ -3,6 +3,12 @@ from django.core.management.base import BaseCommand
 
 
 class Command(BaseCommand):
+    """
+    필요한 패키지는 전역에 있어야 합니다
+    이 스크립트는 가상환경을 실행시킬 수 없습니다
+    --OS 에러 발생--
+    """
+
     help = "모든 데이터를 생성합니다."
 
     def handle(self, *args, **options):
