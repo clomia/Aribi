c4e76d780d3983216ef4a1992b4ec7ce8b2c5cd6
cloe001000 <69475099+cloe001000@users.noreply.github.com>
2021-10-10 22:39:39 +0900

    intro sort-btn 변경

--- static/js/intro/5-sort_btns.js
+++ static/js/intro/5-sort_btns.js
@@ -1,24 +1,65 @@
-const btnDiv = document.querySelector(".sort-btn"),
-    latestBtn = document.querySelector(".sort-btn__latest"),
-    popularityBtn = document.querySelector(".sort-btn__popularity"),
+const sortBtn = document.querySelector(".sort-btn"),
+    sortBtnButton = document.querySelector(".sort-btn button"),
     latestPostings = document.querySelector(".postings-section__latest"),
     popularityPostings = document.querySelector(".postings-section__popularity");
 
-latestBtn.addEventListener("click", function (event) {
-    if (!latestBtn.classList.contains("sort-btn__selected")) {
-        latestBtn.classList.add("sort-btn__selected");
-        popularityBtn.classList.remove("sort-btn__selected");
+function detectMobileDevice(agent) {
+    const mobileRegex = [
+        /Android/i,
+        /iPhone/i,
+        /iPad/i,
+        /iPod/i,
+        /BlackBerry/i,
+        /Windows Phone/i
+    ]
+
+    return mobileRegex.some(mobile => agent.match(mobile))
+}
+
+const isMobile = detectMobileDevice(window.navigator.userAgent)
+
+sortBtn.addEventListener("click", function (event) {
+    if (sortBtn.classList.contains("latest")) {
+        // 최신순 정렬
+        sortBtn.classList.add("popularity");
+        sortBtn.classList.remove("latest");
+        latestPostings.classList.add("none");
+        popularityPostings.classList.remove("none");
+        if (!isMobile) {
+            sortBtnButton.innerHTML = "인기 포스팅"
+        }
+    } else {
+        // 인기순 정렬
+        sortBtn.classList.remove("popularity");
+        sortBtn.classList.add("latest");
         latestPostings.classList.remove("none");
         popularityPostings.classList.add("none");
+        if (!isMobile) {
+            sortBtnButton.innerHTML = "최신 포스팅"
+        }
     }
 })
 
-popularityBtn.addEventListener("click", function (event) {
-    if (!popularityBtn.classList.contains("sort-btn__selected")) {
-        popularityBtn.classList.add("sort-btn__selected");
-        latestBtn.classList.remove("sort-btn__selected");
-        popularityPostings.classList.remove("none");
-        latestPostings.classList.add("none");
+sortBtn.addEventListener("mouseenter", function (event) {
+    if (!isMobile) {
+        if (sortBtn.classList.contains("latest")) {
+            // 최신순 정렬
+            sortBtnButton.innerHTML = "인기 포스팅 보기"
+        } else {
+            // 인기순 정렬
+            sortBtnButton.innerHTML = "최신 포스팅 보기"
+        }
     }
 })
 
+sortBtn.addEventListener("mouseleave", function (event) {
+    if (!isMobile) {
+        if (sortBtn.classList.contains("latest")) {
+            // 최신순 정렬
+            sortBtnButton.innerHTML = "최신 포스팅"
+        } else {
+            // 인기순 정렬
+            sortBtnButton.innerHTML = "인기 포스팅"
+        }
+    }
+})
