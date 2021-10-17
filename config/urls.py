c6409ae7291eb16b78eaaf8307442ac1394615a6
cloe001000 <69475099+cloe001000@users.noreply.github.com>
2021-10-17 10:00:46 +0900

    admin url 변경

--- config/urls.py
+++ config/urls.py
@@ -13,6 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+import os
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
@@ -31,7 +32,7 @@ urlpatterns = [
     path("postings/", include("postings.urls", namespace="postings")),
     path("archives/", include("archives.urls", namespace="archives")),
     path("lists/", include("lists.urls", namespace="lists")),
-    path("admin/", admin.site.urls),
+    path(f"{os.environ.get('ADMIN_URL')}/", admin.site.urls),
     path("ValueError/", value_error),
 ]
 
