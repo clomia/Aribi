d8641d914ef1ac5a070e253f98f6d0f398b7d705
cloe001000 <69475099+cloe001000@users.noreply.github.com>
2021-10-18 00:30:08 +0900

    check

--- config/custom_storages.py
+++ config/custom_storages.py
@@ -2,9 +2,8 @@ from storages.backends.s3boto3 import S3Boto3Storage
 
 
 class StaticStorage(S3Boto3Storage):
-    location = "static/"
-    file_overwrite = False
+    pass
 
 
 class UploadStorage(S3Boto3Storage):
-    location = "upload/"
+    pass
