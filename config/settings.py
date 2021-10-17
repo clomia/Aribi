bb0624c5f2b2582b1b3365878c64766598a5783a
cloe001000 <69475099+cloe001000@users.noreply.github.com>
2021-10-17 09:25:08 +0900

    createsu

--- config/settings.py
+++ config/settings.py
@@ -88,24 +88,14 @@ WSGI_APPLICATION = "config.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# if DEBUG:
-#     DATABASES = {
-#         "default": {
-#             "ENGINE": "django.db.backends.sqlite3",
-#             "NAME": BASE_DIR / "db.sqlite3",
-#         }
+
+# ------------------ DEBUG ------------------
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
 #     }
-# else:
-#     DATABASES = {
-#         "default": {
-#             "ENGINE": "django.db.backends.postgresql",
-#             "HOST": os.environ.get("RDS_HOST"),
-#             "NAME": os.environ.get("RDS_NAME"),
-#             "USER": os.environ.get("RDS_USER"),
-#             "PASSWORD": os.environ.get("RDS_PASSWORD"),
-#             "PORT": "5432",
-#         }
-#     }
+# }
 
 # ----------- AWS Deploy--------------
 DATABASES = {
